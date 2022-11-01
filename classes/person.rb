ca5b33c5034107bfbfe82783f7e610a8576b1147
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-01 20:44:07 +0430

    Update person.rb add varables

--- classes/person.rb
+++ classes/person.rb
@@ -1,4 +1,9 @@
 class Person
   attr_accessor id: :name, :age, :parent_permission
 
-  
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
