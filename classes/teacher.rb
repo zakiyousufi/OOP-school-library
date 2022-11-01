cc9b5df8ea544833ff31079422746d4c18692e94
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-01 21:14:43 +0430

    Update teacher.rb add initialize def

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -2,4 +2,10 @@ require_relative 'person'
 
 class Teacher < person
   attr_accessor :specialization
+
+  def initialize(age, specialization, name: 'unknown')
+    super(age, name)
+    @specialization = specialization
+  end
+  
 end
