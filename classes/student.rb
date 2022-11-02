a452e50d2c606fdd8986b303c81c216d923347b2
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:31:40 +0430

    Update studentsrb

--- classes/student.rb
+++ classes/student.rb
@@ -3,12 +3,16 @@ require_relative 'Person'
 class Student < Person
   attr_accessor :classroom
 
+
+  def initialize(age, classroom = 5, name: 'unknown', parent_permission: true)
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
+
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    "¯\(ツ)/¯"
   end
 end
