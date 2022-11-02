1a9340b6f52f739d8189bd8c0c3a679d1d7c844f
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-03 00:55:15 +0430

    Apply requsted changes on person.rb

--- classes/person.rb
+++ classes/person.rb
@@ -1,27 +1,28 @@
 require_relative 'Nameable'
 
 class Person < Nameable
-  attr_accessor :id, :name, :age, :parent_permission
+  attr_accessor :name, :age, :parent_permission, :rentals, :id
+  attr_reader :corrector
 
-  def initialize(age, name, parent_permission: true)
-    super()
-    @id = Random.rand(1...1000)
+  def initialize(name, age, parent_permission = { parent_permission: true })
+    @id = rand(1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
-
-  def can_use_services?
-    if of_age? || @parent_permission
-      true
-    else
-      false
-    end
-  end
-
-  private
 
   def of_age?
     @age >= 18
   end
+
+  def can_use_services?
+    of_age && parent_permission[:parent_permission]
+  end
+
+  def validate_name
+    @name = @corrector.correct_name
+  end
+
+  private :of_age?
 end
