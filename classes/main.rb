90ed79eb85a35b7c9c8f5168f74261366225d12d
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:37:51 +0430

    Update main.rb

--- classes/main.rb
+++ classes/main.rb
@@ -3,9 +3,7 @@ require_relative 'decorator'
 
 person = Person.new(10, 'lazycoders')
 puts person.correct_name
-
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
-
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
