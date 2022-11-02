556f9db42946e63f481447b2229f550f4684973b
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:17:51 +0430

    Add "def correct_name" on class TrimmerDecorator on decorate.rb

--- decorate.rb
+++ decorate.rb
@@ -18,5 +18,7 @@ class CapitalizeDecorator < BaseDecorator
 end
 
 class TrimmerDecorator < BaseDecorator
-  
+  def correct_name
+    @nameable.correct_name[0, 10] if @nameable.correct_name.length > 10
+  end
 end
