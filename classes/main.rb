5d216080ff6e3de6f36e30eb381b812771f64ca8
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:36:09 +0430

    Update main.rb add capitalized_person

--- classes/main.rb
+++ classes/main.rb
@@ -3,3 +3,6 @@ require_relative 'decorator'
 
 person = Person.new(10, 'lazycoders')
 puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
