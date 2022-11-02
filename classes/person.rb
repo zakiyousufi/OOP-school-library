2e86054e43b9912ae2b85ae0208019265d4bad1a
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:29:41 +0430

    Update person.rb and relocate decorate.rb

--- classes/person.rb
+++ classes/person.rb
@@ -1,7 +1,10 @@
-class Person
+require_relative 'Nameable'
+
+class person < Nameable
   attr_accessor :id, :name, :age, :parent_permission
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize def initialize(age, name, parent_permission: true)
+    super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
