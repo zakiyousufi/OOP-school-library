6d0b066712747e65d45d32cce7f5bde760ea3609
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-02 22:37:19 +0430

    Update main.rb add capitalized_trimmed_person

--- classes/main.rb
+++ classes/main.rb
@@ -6,3 +6,6 @@ puts person.correct_name
 
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
