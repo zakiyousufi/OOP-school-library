3a52a0c96c146edef75cdc801746080dd75d1ad1
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-03 13:34:53 +0430

    Create class Book in book.rb

---
+++ classes/book.rb
@@ -0,0 +1,5 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+end
+  
--- classes/person.rb
+++ classes/person.rb
@@ -1,26 +1,38 @@
-require './nameable'
-class Person < Nameable
-  attr_reader :id, :name, :age
+require_relative 'nameable'
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.rand(0..1000)
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age, name, parent_permission: true)
+    super()
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    super()
+    @rentals = []
   end
 
-  def of_age?
-    @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || parent_permission
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 
   def correct_name
     @name
   end
 
-  private :of_age?
+  def can_use_services?
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
