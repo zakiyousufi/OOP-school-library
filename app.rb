4b22de609d5df0ce375a0b224d7854f10706d330
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-04 18:43:41 +0430

    Add def initialize in app.rb

--- app.rb
+++ app.rb
@@ -4,3 +4,20 @@ require './classes/rental'
 require './classes/student'
 require './classes/teacher'
 require './classes/classroom'
+
+class App
+    attr_accessor :books_list, :people
+  
+    def initialize
+      @books_list = []
+      @people = []
+      @rentals = []
+      @students = []
+      @teachers = []
+    end
+  
+    def run
+      display_list
+    end
+end
+  
