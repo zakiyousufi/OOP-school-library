2aa64f07b766085fc560d1fe642b4091b2708eb8
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-04 18:48:03 +0430

    Add list of peoples on app.rb

--- app.rb
+++ app.rb
@@ -6,27 +6,37 @@ require './classes/teacher'
 require './classes/classroom'
 
 class App
-    attr_accessor :books_list, :people
+  attr_accessor :books_list, :people
+
+  def initialize
+    @books_list = []
+    @people = []
+    @rentals = []
+    @students = []
+    @teachers = []
+  end
   
-    def initialize
-      @books_list = []
-      @people = []
-      @rentals = []
-      @students = []
-      @teachers = []
-    end
-  
-    def run
-      display_list
-    end
+  def run
+    display_list
+  end
     
-    # ======= show list all the books =======
-    def list_all_books
-      if @books_list.empty?
-        puts 'There are no books in the library'
-      else
-        @books_list.each_with_index { |book, i| puts "#{i}) Title: '#{book.title}', Author: #{book.author}" }
+# ======= show list all the books =======
+  def list_all_books
+    if @books_list.empty?
+      puts 'There are no books in the library'
+    else
+      @books_list.each_with_index { |book, i| puts "#{i}) Title: '#{book.title}', Author: #{book.author}" }
+    end
+  end
+# ======= show list all the peoples =======
+  def list_all_people
+    if @people.empty?
+      puts 'There are no people in the library'
+    else
+      @people.each_with_index do |person, i|
+      puts "#{i}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
     end
+  end    
 end
   
