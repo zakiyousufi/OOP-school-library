8b0b222e87963162cbae53f6a34d64bdb968ef06
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-04 18:16:51 +0430

    update person.rb

--- classes/person.rb
+++ classes/person.rb
@@ -13,9 +13,8 @@ class Person < Nameable
     @rentals = []
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.person = self
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 
   def correct_name
