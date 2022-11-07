64ff941efdd3f846eeda133251336b119ff5400b
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-11-07 21:23:47 +0430

    Add id in initialize def on teacher.rb

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,10 +1,13 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, name, specialization)
-    super(age, name)
+  def initialize(age, id, name, specialization)
+    super(age, id, name)
     @specialization = specialization
   end
+
+  attr_reader :parent_permission
+  attr_accessor :specialization
 
   def can_use_services?
     true
