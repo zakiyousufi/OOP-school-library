c95baf2ccc23c6bd9a4c709ddc2fc2d3fb795ed3
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-07 23:30:22 +0530

    Edit preserve.rb file for methods load_people, save_rental and load_rentalsand check linters

--- classes/preserve.rb
+++ classes/preserve.rb
@@ -50,3 +50,43 @@ class Preserve
       File.write('people.json', JSON.pretty_generate([new_person]))
     end
   end
+
+  def load_people
+    return unless File.exist?('people.json')
+
+    people_loaded = JSON.parse(File.read('people.json'))
+    people_loaded.each do |person|
+      case person['type']
+      when 'student'
+        new_person = Student.new(nil, person['id'], person['age'], person['name'], person['parent_permission'])
+        @people << new_person
+      when 'teacher'
+        new_person = Teacher.new(person['specialization'], person['id'], person['age'], person['name'])
+        @people << new_person
+      end
+    end
+  end
+
+  def save_rental(rental)
+    if File.exist?('rentals.json')
+      rentals_loaded = JSON.parse(File.read('rentals.json'))
+      rentals_loaded << { date: rental.date, person_id: rental.person.id, book_id: rental.book.id }
+      File.write('rentals.json', JSON.pretty_generate(rentals_loaded))
+    else
+      File.write('rentals.json',
+                 JSON.pretty_generate([{ date: rental.date, person_id: rental.person.id, book_id: rental.book.id }]))
+    end
+  end
+
+  def load_rentals
+    return unless File.exist?('rentals.json')
+
+    rentals_loaded = JSON.parse(File.read('rentals.json'))
+    rentals_loaded.each do |rental|
+      book = @books.select { |x| x.id == rental['book_id'] }[0]
+      person = @people.select { |x| x.id == rental['person_id'] }[0]
+      new_rental = Rental.new(rental['date'], book, person)
+      @rentals << new_rental
+    end
+  end
+end
